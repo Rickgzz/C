d3f35f23a46f9706a919169ead4414fc31a3f132
Rickgzz <78566444+Rickgzz@users.noreply.github.com>
2021-02-24 09:56:08 -0600

    Update homework1.c

--- homework/homework1.c
+++ homework/homework1.c
@@ -4,30 +4,45 @@
 #define IN 1
 #define OUT 0
 // NO FUNCIONO :(
+char quitar(char);
 
 int main() {
     
-  int i, l, n;
-  
-  char input[MAX];
-  char output[MAX];
+      int i, l, n;
       
-  printf("Insertar texto formato HTML: ");
-  gets(input);
-  l = strlen(input);
-  n = OUT;
-
-  for (i = 0; i<l ; ++i) {
+      char input[MAX];
+      char output[MAX];
+     
+      printf("Insertar texto formato HTML: ");
+      gets(input);
+      l = strlen(input);
+      n = OUT;
+      
+     printf("\nTexto plano: ");
+     
+     for (i = 0; i<l ; ++i) {
           
-    if (input[i] == '<')
-      n = IN;
-      if (input[i] == '>')
-        n = OUT;       
-    
-    if (n == IN)
-      output[i] = input[i];   //checar esta parte
+          if (input[i] == '<')
+             n = IN;
+          if (input[i] == '>')
+             n = OUT;       
+          if (n == OUT)
+             output[i] = input[i];
+             //quitar(output[i]);
+             printf("%c",output[i]); // checar  
+          }
+     
+     }
+
+
+char quitar(char) {
+     int m;
+     
+     for (m = 0; m<30 ; ++m) {
+         
+         if (output[m]=='<')
+             (output[m] = '_');
+             }
+             
+     return output[m];
     }
-
-  printf("\nTexto plano: %d", output);
-
-}
