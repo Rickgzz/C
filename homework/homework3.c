8460c056de7aed8f72361f44966908b9d0b8f365
Rickgzz <78566444+Rickgzz@users.noreply.github.com>
2021-03-09 18:24:23 -0600

    Update homework3.c

--- homework/homework3.c
+++ homework/homework3.c
@@ -5,20 +5,27 @@
 
 main(){
 
-  int c;
+  int c,i=0;
   char str[100];
-   
-  for(int i=0;(c = getchar()) != EOF;) {  //almacenar getchar en un array
-    str[i++] = c;
-    str[i] = '\0';
+
+
+  while((c = getchar()) != EOF) { 
+
+    switch (c){                       //elimina todos las puntuaciones
+      case 'a': case 'c': case 'e': case 'g': case 'i': case 'k': case 'm': case 'o': case 'q': case 's': case 'u': case 'w': case 'y': case '(': case ')': case ' ': case '"':
+      case 'b': case 'd': case 'f': case 'h': case 'j': case 'l': case 'n': case 'p': case 'r': case 't': case 'v': case 'x': case 'z': case '{': case '}': case '0': case '2': case '4': case '6': case '8':
+      case 'A': case 'C': case 'E': case 'G': case 'I': case 'K': case 'M': case 'O': case 'Q': case 'S': case 'U': case 'W': case 'Y': case '[': case ']': case '1': case '3': case '5': case '7': case '9': 
+      case 'B': case 'D': case 'F': case 'H': case 'J': case 'L': case 'N': case 'P': case 'R': case 'T': case 'V': case 'X': case 'Z': case '\n': case '\t':
+        str[i++] = c;
+        str[i] = '\0';
+    }   
   }
 
   int l = strlen(str);
-  int count =0, par=OUT;
-  int d=2;
+  int d =2, par=OUT;
 
 
-  for (int i = 0; i < l; i++){    //si estan dentro de un parentesis no pasa nada
+  for (int i = 0; i < l; i++){        //si estan dentro de un parentesis no pasa nada
 
     if(str[i]=='(' || str[i]=='{' || str[i]=='['){
       par=IN;
@@ -26,14 +33,13 @@ main(){
     if(str[i]==')' || str[i]=='}' || str[i]==']'){
       par=OUT;    
     }
-
-    if (str[i]=='"'){            //si estan dentro de unas comillas no pasa nada
+    if (str[i]=='"'){                  //si estan dentro de unas comillas no pasa nada
       d++;
     }
 
-    if(par==OUT && (d%2)==0){                 
+    if(par==OUT && (d%2)==0){          
 
-      switch(str[i]){             //cambiar los numeros por X
+      switch(str[i]){                  //cambiar los numeros por X
             
       case '0': case '2': case '4': case '6': case '8':
       case '1': case '3': case '5': case '7': case '9': 
@@ -43,28 +49,12 @@ main(){
     }
   }
  
-
-  for(int i=0,j=0;i<l+1;i++){         //elimina las agrupaciones de X
+  for(int i=0,j=0;i<l+1;i++){           //elimina las agrupaciones de X
         
     if ((str[i] != 'X') || (str[i+1] != 'X')){
       str[j++] = str[i];
     }
   }
-  
-  l = strlen(str);
 
-  for (int i = 0, j = 0; i < l; i++){   //checar esta parte //la longitud de el array
-
-    switch (str[i]){                  //elimina todos las puntuaciones
-      case 'a': case 'c': case 'e': case 'g': case 'i': case 'k': case 'm': case 'o': case 'q': case 's': case 'u': case 'w': case 'y': case '(': case ')': case ' ': case '"':
-      case 'b': case 'd': case 'f': case 'h': case 'j': case 'l': case 'n': case 'p': case 'r': case 't': case 'v': case 'x': case 'z': case '{': case '}': case '0': case '2': case '4': case '6': case '8':
-      case 'A': case 'C': case 'E': case 'G': case 'I': case 'K': case 'M': case 'O': case 'Q': case 'S': case 'U': case 'W': case 'Y': case '[': case ']': case '1': case '3': case '5': case '7': case '9': 
-      case 'B': case 'D': case 'F': case 'H': case 'J': case 'L': case 'N': case 'P': case 'R': case 'T': case 'V': case 'X': case 'Z': case '\n': case '\t':
-        str[j++] = str[i];
-        break;
-
-    }
-  }
-
-  printf("%s\n", str); //imprimir el array modificado
+  printf("%s", str);                    //imprimir el array modificado
 }
