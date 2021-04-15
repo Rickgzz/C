b42c1193f21256a96fb4309ae9699f1db89441fe
Rickgzz <78566444+Rickgzz@users.noreply.github.com>
2021-04-15 08:09:28 -0500

    Add files via upload

---
+++ homework/homework4/header.h
@@ -0,0 +1,9 @@
+#define MAX 100  
+#define NUMBER '0'
+
+int 	getop(char []); 		
+void 	push(double); 			
+double 	pop(void); 				
+int 	options(int, char[]); 	
+int 	getch(void); 			   
+void 	ungetch(int);
