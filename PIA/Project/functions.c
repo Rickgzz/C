eb40c9c17d3dfe7f7922a4890a4626145b3aadfd
Rickgzz <78566444+Rickgzz@users.noreply.github.com>
2021-05-19 18:58:28 -0500

    Update functions.c

--- PIA/Project/functions.c
+++ PIA/Project/functions.c
@@ -1,6 +1,6 @@
 #include "header.h"
 
-void SumRes(float* rT, float* rE, int p, int s){       
+void SumRes(float* rT, float* rE, int p, int s){                            //Obtain the Equivalent resistance and Total Resistance
     int i;
 
     for (i=1; i<=s; i++)    *rT += (SERIE[i].resistance);          
@@ -13,7 +13,7 @@ void SumRes(float* rT, float* rE, int p, int s){
     }
 }
 
-void ValSerie(float* aT, int s){
+void ValSerie(float* aT, int s){                                              //Obtain all the individual values of components conected in series
     int i;
 
     for (i=1; i<=s; i++){                                          
@@ -23,10 +23,10 @@ void ValSerie(float* aT, int s){
     }
 }
 
-void ValParallel(float* aE, float* aT, float* vE, float* rE, int p){
+void ValParallel(float* aE, float* aT, float* vE, float* rE, int p){        //Obtain all the individual values of components conected in parallel
     int i;
 
-    if(p>0){                                                  
+    if(p>0){                                                                //get Equivalent values       
         *aE = *aT;
         *vE = (*aE)*(*rE);
     }
@@ -37,15 +37,18 @@ void ValParallel(float* aE, float* aT, float* vE, float* rE, int p){
     } 
 }       
 
-void PrintValues(float vT, float rT, float aT, int p, int s){
+void PrintValues(float vT, float rT, float aT, int p, int s){                     //Print all the values
     int i;
+    printf("\n---------------------------------------------");                     //Print total values
 
     printf("\n\nThe total Voltage is: %.2f volts\n", vT);       
     printf("The total Resistance is: %.2f ohms\n", rT);
     printf("The total Current is: %.2f amperes\n", aT); 
 
+    printf("\n---------------------------------------------\n");
+
     printf("\nVoltage[V]  Resistance[R]  Current[I]\n");
-    printf("\nValues of the components in SERIES:\n");
+    printf("\nValues of the components in SERIES:\n");                         //Print values in series
     for(i=1; i<=s; i++){
 
         printf("Component(%d): [V:%4.1f] [R:%4.1f] [I:%4.1f]\n", SERIE[i].num, SERIE[i].voltage, SERIE[i].resistance, SERIE[i].current );
@@ -53,10 +56,12 @@ void PrintValues(float vT, float rT, float aT, int p, int s){
     
     if (p>0){
 
-        printf("\nValues of the components in PARALLEL:\n");
+        printf("\nValues of the components in PARALLEL:\n");                    //Print values parallel
         for(i=1; i<=p; i++){
 
             printf("Component(%d): [V:%4.1f] [R:%4.1f] [I:%4.1f]\n", PARALLEL[i].num, PARALLEL[i].voltage, PARALLEL[i].resistance, PARALLEL[i].current );
         }
     }
+    printf("\n---------------------------------------------\n");
+
 }
